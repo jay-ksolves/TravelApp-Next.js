4e5fd88c5af854f69720333df120e63a134ac8db
jay-ksloves <jay.prakash@ksolves.com>
2023-11-04 20:49:44 +0530

    added login

---
+++ app/login/page.tsx
@@ -0,0 +1,59 @@
+// import React, { useState } from 'react';
+// import { useRouter } from 'next/router';
+import Link from 'next/link';
+import React from 'react'
+function LoginPage() {
+    // const router = useRouter();
+    // const [user, setUser] = useState({
+    //     email: '',
+    //     password: '',
+    // });
+
+    //   const onLogin = async () => {
+    //     // Add your login logic here
+    //     // You can use 'user.email' and 'user.password' to access user input
+    //   };
+
+    return (
+        <div className="bg-gray-100  flex items-center justify-center mx-auto max-w-[1440px]">
+            <div className="glass p-5 m-20 text-light">
+                <h1 className="text-center font-lobster text-3xl">Welcome to Login page</h1>
+                <br />
+                <div className="bg-dark p-3 rounded-lg max-w-sm mx-auto">
+                    <label htmlFor="EmailId">Email Id</label>
+                    <input
+                        type="email"
+                        id="EmailId"
+                        placeholder="Email Id"
+                        className="w-full px-4 py-2 rounded-lg focus:ring focus:ring-indigo-400"
+                    // value={user.email}
+                    // onChange={(e) => setUser({ ...user, email: e.target.value })}
+                    />
+                    <label htmlFor="Password">Password</label>
+                    <input
+                        type="password"
+                        id="Password"
+                        placeholder="Password"
+                        className="w-full px-4 py-2 rounded-lg focus:ring focus:ring-indigo-400"
+                    // value={user.password}
+                    // onChange={(e) => setUser({ ...user, password: e.target.value })}
+                    />
+                    <button
+                        // onClick={onLogin}
+                        className="block bg-indigo-600 text-white rounded-lg px-4 py-2 mt-3 mx-auto hover:bg-indigo-700 focus:ring focus:ring-indigo-400"
+                    >
+                        Login Now
+                    </button>
+                    <hr  className='mt-5'/>
+                    <Link href="/signup" className="block text-center text-light hover:underline mt-5" >
+                        <button style={{ textDecoration: 'none' }}>
+                            Visit Signup Page
+                        </button>
+                    </Link>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default LoginPage;
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,36 +1,48 @@
-import { NAV_LINKS } from '@/constants';
-import Image from 'next/image'
-import Link from 'next/link';
-import React from 'react';
-import Button from './Button';
+import { NAV_LINKS } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import Button from "./Button";
 
 const Navbar = () => {
-    return (
-        <nav className='mx-auto max-w-[1440px] padding-container relative z-30 py-5 p-5 flex justify-between items-center'>
-            <Link href='/'>
-                <img src='logo.png' alt='logo' width={160} height={100} className=' ' />
-                {/* <Image src='TrekEase-logo.svg' alt='logo' width={74} height={29} /> */}
-            </Link>
-            <ul className='h-full gap-12 lg:flex hidden'>
-                {NAV_LINKS.map(link => (
-                    <Link href={link.href} key={link.key} className='font-light text-gray-500 flex-center cursor-pointer pb-1.5 transition-all hover:font-bold  hover:text-neutral-800  hover:underline hover:decoration-solid' style={{ textDecorationColor: 'orange' }}>
-                        {link.label}
-                    </Link>
-                ))}
-            </ul>
-            <div className='lg:flex-center'>
-                <Button
-                    type="button"
-                    title="Login"
-                    icon='user.svg'
-                    variant='bg-black p-2 px-3 text-gray-300 flex transition-all hover:bg-cyan-500 hover:text-blue-200 hover:border-light-900 '
-                />
-            </div>
-            <Image src='menu.svg' alt='menu' width={32} height={32} className='inline-block cursor-pointer lg:hidden' />
-        </nav>
-
-
-    );
-}
+  return (
+    <nav className="mx-auto max-w-[1440px] padding-container relative z-30 py-5 p-5 flex justify-between items-center">
+      <Link href="/">
+        <img src="logo.png" alt="logo" width={160} height={100} className=" " />
+        {/* <Image src='TrekEase-logo.svg' alt='logo' width={74} height={29} /> */}
+      </Link>
+      <ul className="h-full gap-12 lg:flex hidden">
+        {NAV_LINKS.map((link) => (
+          <Link
+            href={link.href}
+            key={link.key}
+            className="font-light text-gray-500 flex-center cursor-pointer pb-1.5 transition-all hover:font-bold  hover:text-neutral-800  hover:underline hover:decoration-solid"
+            style={{ textDecorationColor: "orange" }}
+          >
+            {link.label}
+          </Link>
+        ))}
+      </ul>
+      <div className="lg:flex-center">
+        <Link href="/login">
+          {" "}
+          <Button
+            type="button"
+            title="Login"
+            icon="user.svg"
+            variant="bg-black p-2 px-3 text-gray-300 flex transition-all hover:bg-cyan-500 hover:text-blue-200 hover:border-light-900 "
+          />
+        </Link>
+      </div>
+      <Image
+        src="menu.svg"
+        alt="menu"
+        width={32}
+        height={32}
+        className="inline-block cursor-pointer lg:hidden"
+      />
+    </nav>
+  );
+};
 
 export default Navbar;
